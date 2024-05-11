aae0e3dc77d3271a3fc9a493bc44e4f483249d77
vidaldl <vidaldl@outlook.com>
2024-05-11 15:12:07 -0600

    Added task interface

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { Task } from './task'; // Import the Task interface
+
+let tasks: Task[] = []; 
---
+++ src/task.ts
@@ -0,0 +1,10 @@
+// This is the definition of the interface
+interface Task {
+    id: number;
+    description: string;
+    completed: boolean;
+    priority?: "low" | "medium" | "high";  // Optional
+    dueDate?: Date;  // Optional 
+}
+
+export { Task };
